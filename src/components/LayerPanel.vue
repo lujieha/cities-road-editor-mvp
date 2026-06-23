62b3b801b8c7c9dfb7d86496125717bf7267dbf5
lujieha <lujieh@163.com>
2026-06-23 11:35:18 +0800

    feat: add layer panel

---
+++ src/components/LayerPanel.vue
@@ -0,0 +1,2 @@
+<template><div class="toolbar-block"><div class="panel-title">渲染分层</div><div class="small">ground / roadSurface / lane / marking / intersection / sign / facility / preview / helper / selection</div></div></template>
+<script setup lang="ts"></script>
