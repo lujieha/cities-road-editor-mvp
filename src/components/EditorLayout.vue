71603f75c25d4965893611ebf9c5d4c8714f41b0
lujieha <lujieh@163.com>
2026-06-23 10:22:48 +0800

    feat: add editor layout

---
+++ src/components/EditorLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="editor-shell">
+    <aside class="left-panel">
+      <RoadToolbar />
+      <BuildModePanel />
+      <RoadTypePanel />
+      <LayerPanel />
+      <PropertyPanel />
+    </aside>
+    <main class="viewport-wrap"><ThreeViewport /><StatusOverlay /></main>
+  </div>
+</template>
+<script setup lang="ts">
+import ThreeViewport from './ThreeViewport.vue'
+import RoadToolbar from './RoadToolbar.vue'
+import RoadTypePanel from './RoadTypePanel.vue'
+import BuildModePanel from './BuildModePanel.vue'
+import StatusOverlay from './StatusOverlay.vue'
+import LayerPanel from './LayerPanel.vue'
+import PropertyPanel from './PropertyPanel.vue'
+</script>
